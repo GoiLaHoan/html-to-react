91a125f3c30034062c015392d528321c21e0be46
NVH <nvhoan911@gmail.com>
2022-02-28 10:10:54 +0700

    update loading

--- src/components/layout/layout.component.js
+++ src/components/layout/layout.component.js
@@ -1,5 +1,6 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
 import { Loading } from "../Loading/Loading";
+import BackToTop from "../BackToTop";
 import Routes from "../../routes/routes";
 import Footer from "../Header&Footer/Footer";
 import Sidebar from "../Sidebar/Sidebar.js";
@@ -7,31 +8,40 @@ import Header from "../Header&Footer/Header";
 import { OpenContext } from "../Context/OpenContext";
 
 const LayoutCompnent = () => {
-  const showSidebar = useContext(OpenContext);
-  const [show] = useState(false);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    if (loading) {
-      setTimeout(() => {
-        setLoading(false);
-      }, 1000);
-    }
-  }, [loading]);
+    const showSidebar = useContext(OpenContext);
+    const [show] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const refScrollUp = useRef();
 
-  if (loading) return <Loading />;
-  return (
-    <>
-      <div className="container-xxl position-relative bg-white d-flex p-0">
-        <Sidebar />
-        <div className={`content ${showSidebar.open}`}>
-          {show}
-          <Header />
-          <Routes />
-          <Footer />
-        </div>
-      </div>
-    </>
-  );
+    const handleScrollUp = () => {
+        refScrollUp.current.scrollIntoView({ behavior: "smooth" });
+    };
+
+    useEffect(() => {
+        if (loading) {
+            setTimeout(() => {
+                setLoading(false);
+            }, 500);
+        }
+    }, [loading]);
+
+    if (loading) return <Loading />;
+    return (
+        <>
+            <div className="container-xxl position-relative bg-white d-flex p-0">
+                <Sidebar />
+                <div className={`content ${showSidebar.open}`}>
+                    <div ref={refScrollUp}>
+                        <BackToTop scrollUp={handleScrollUp} />
+                    </div>
+                    {show}
+                    <Header />
+                    <Routes />
+                    <Footer />
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default LayoutCompnent;
