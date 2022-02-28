a0e90b0962b195038a528b26345e7f45fe496cbf
DungRoman <nguyenducdung853@gmail.com>
2022-02-28 09:46:09 +0700

    dung commit loading

--- src/components/layout/layout.component.js
+++ src/components/layout/layout.component.js
@@ -1,24 +1,37 @@
-import { useContext } from "react";
-
+import React, { useContext, useState, useEffect } from "react";
+import { Loading } from "../Loading/Loading";
 import Routes from "../../routes/routes";
 import Footer from "../Header&Footer/Footer";
 import Sidebar from "../Sidebar/Sidebar.js";
 import Header from "../Header&Footer/Header";
 import { OpenContext } from "../Context/OpenContext";
+
 const LayoutCompnent = () => {
-    const showSidebar = useContext(OpenContext);
-    return (
-        <>
-            <div className="container-xxl position-relative bg-white d-flex p-0">
-                <Sidebar />
-                <div className={`content ${showSidebar.open}`}>
-                    <Header />
-                    <Routes />
-                    <Footer />
-                </div>
-            </div>
-        </>
-    );
+  const showSidebar = useContext(OpenContext);
+  const [show] = useState(false);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    if (loading) {
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    }
+  }, [loading]);
+
+  if (loading) return <Loading />;
+  return (
+    <>
+      <div className="container-xxl position-relative bg-white d-flex p-0">
+        <Sidebar />
+        <div className={`content ${showSidebar.open}`}>
+          {show}
+          <Header />
+          <Routes />
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LayoutCompnent;
